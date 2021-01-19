437547b3844c100923f77ad64f6e970b593cc92d
Asadullah Iqbal <asadullah.iqbal@live.com>
2021-01-20 02:15:51 +0500

    Add new CommetteList Widget

---
+++ lib/src/CommetteList.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class CommetteListView extends StatelessWidget {
+  CommetteListView(List<String> this._list);
+
+  List<String> _list = [];
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Committee List',
+          style: TextStyle(color: Colors.black, fontSize: 20),
+        ),
+        backgroundColor: Colors.orange,
+      ),
+      backgroundColor: Colors.orange,
+      body: _ListView(context),
+    );
+  }
+
+  Widget _ListView(BuildContext context) {
+    return ListView.separated(
+      itemCount: _list.length,
+      itemBuilder: (context, index) {
+        return Card(
+          shadowColor: Colors.grey,
+            child: ListTile(
+              onTap: () {},
+              title: Text(_list[index]),
+              subtitle: Text("Month : January \n Amount : 10,000.00"),
+            ),
+          );
+      },
+      separatorBuilder: (context, index) {
+        return Divider();
+      },
+    );
+  }
+}
